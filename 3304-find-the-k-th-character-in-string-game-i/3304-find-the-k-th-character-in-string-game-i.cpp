0c8d1199db1706d7136877e68927b54dee01b226
RPS2610 <rudra.25b01010986@abes.ac.in>
2026-09-07 09:10:22 +0530

    Time: 3 ms (44.02%), Space: 10.1 MB (25%) - LeetHub

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+
+        while (word.length() < k) {
+            string next = "";
+
+            for (char c : word) {
+                next += char(c + 1);
+            }
+
+            word += next;
+        }
+
+        return word[k - 1];
+    }
+};
