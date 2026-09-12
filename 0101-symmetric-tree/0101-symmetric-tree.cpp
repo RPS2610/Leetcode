dacaeef3af7378f3ede83355bb6f8f5821b5108e
RPS2610 <rudra.25b01010986@abes.ac.in>
2026-09-12 09:58:57 +0530

    Time: 0 ms (100%), Space: 18.5 MB (73.38%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool check(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+
+        if (p == NULL || q == NULL)
+            return false;
+
+        if (p->val != q->val)
+            return false;
+
+        return check(p->left, q->right) &&
+               check(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left, root->right);
+    }
+};
